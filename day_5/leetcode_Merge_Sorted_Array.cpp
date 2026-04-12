5b54316d856fdfb07fc8235a34c5ffb38ba12e48
Simran Singh <simransinghh2120@gmail.com>
2026-04-12 22:58:58 +0530

    Day 5: Leetcode - Merge Sorted Array

---
+++ day_5/leetcode_Merge_Sorted_Array.cpp
@@ -0,0 +1,52 @@
+/* You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+and the last n elements are set to 0 and should be ignored. nums2 has a length of n. */
+
+#include <iostream>
+using namespace std;
+
+void merge(int nums1[], int m, int nums2[], int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+
+    // Merge from back
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // If nums2 still has elements
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+
+int main() {
+    int nums1[6] = {1, 2, 3, 0, 0, 0};
+    int nums2[3] = {2, 5, 6};
+
+    int m = 3, n = 3;
+
+    merge(nums1, m, nums2, n);
+
+    cout << "Merged array: ";
+    for (int i = 0; i < m + n; i++) {
+        cout << nums1[i] << " ";
+    }
+
+    return 0;
+}
